b904bee8e7cb89f2ba55f5dc5cadc6d1bb47336a
Sebastian <sebastian.varelagi@gmail.com>
2024-11-29 14:53:39 -0500

    Update Controls.tsx
    Add round number to this component.

--- src/app/components/Controls.tsx
+++ src/app/components/Controls.tsx
@@ -8,6 +8,7 @@ import { Button } from "./Button"
 import { Modal } from "./Modal"
 import { ModalSettingsContent } from "./ModalSettingsContent";
 import { PlayIcon } from "../../../public/icons/PlayIcon";
+import { useAppSelector } from "@/redux/hooks";
 
 interface controlsProps {
   ticking: boolean;
@@ -16,6 +17,8 @@ interface controlsProps {
 
 export const Controls = ({ticking, toggleTime}: controlsProps) => {
 
+  const {mode, round } = useAppSelector(state => state.timer)
+
   const [showModal, setShowModal] = useState(false)
 
   const openModal = () =>{
@@ -23,28 +26,31 @@ export const Controls = ({ticking, toggleTime}: controlsProps) => {
   }
 
   return (
-    <div className="flex w-full h-auto justify-center items-center">
-      <div className="flex w-fit justify-around text-sm md:text-lg font-bold gap-x-6">  
-        <Button openModal={openModal}>
-          <SettingsIcon/>
-        </Button>
-        
-        <Button toggleTime={toggleTime}>
-          {
-            ticking
-              ? 
-              <PauseIcon/>
-              : 
-              <PlayIcon/>
-          }
+    <div className="flex flex-col gap-6 items-center">
+      <span className="text-2xl font-semibold opacity-70">#{(mode === 'POMODORO') ? round : (round !== 1) ? (round - 1) : '1'}</span>
+      <div className="flex w-full h-auto justify-center items-center">
+        <div className="flex w-fit justify-around text-sm md:text-lg font-bold gap-x-6">  
+          <Button openModal={openModal}>
+            <SettingsIcon/>
           </Button>
-        <Button isConfirm>
-          <SkipIcon/>
-        </Button>
+          
+          <Button toggleTime={toggleTime}>
+            {
+              ticking
+                ? 
+                <PauseIcon/>
+                : 
+                <PlayIcon/>
+            }
+            </Button>
+          <Button isConfirm>
+            <SkipIcon/>
+          </Button>
+        </div>
+        <Modal isVisible={showModal} title="Settings" onClose={() => setShowModal(false)} isConfirm={false}>
+          <ModalSettingsContent onClose={() => setShowModal(false)}/>
+        </Modal>
       </div>
-      <Modal isVisible={showModal} title="Settings" onClose={() => setShowModal(false)} isConfirm={false}>
-        <ModalSettingsContent onClose={() => setShowModal(false)}/>
-      </Modal>
     </div>
   )
 }
